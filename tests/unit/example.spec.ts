3e80fbe223506f5d3d0f189444ad97db04207ee2
Eduardo Wesley <eduardomudutiu@gmail.com>
2020-09-27 20:27:05 -0300

    resolve git conflicts

---
+++ tests/unit/example.spec.ts
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+import { shallowMount } from '@vue/test-utils';
+
+describe('HelloWorld.vue', () => {
+  it('renders props.msg when passed', () => {
+    //
+  });
+});
